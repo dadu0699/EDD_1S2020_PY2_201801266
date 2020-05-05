8eaa33f87d55605b47791a372ed6307e61a9efc5
Didier Domínguez <dadu0699@hotmail.com>
2020-05-04 20:50:00 -0600

    [UPDATE] BTree

--- src/org/didierdominguez/structures/BTree/BTree.java
+++ src/org/didierdominguez/structures/BTree/BTree.java
@@ -453,10 +453,71 @@ public class BTree {
     }
 
     public String getString() {
-        return BTreePrint.getString(this);
+        return getString(this);
     }
 
     public ArrayList<Book> getBooks() {
-        return BTreePrint.getBooks(this.root);
+        return getBooks(this.root);
+    }
+
+    private String getString(BTree tree) {
+        if (tree.root == null) {
+            return "Tree has no nodes.";
+        }
+        return getString(tree.root, "", true);
+    }
+
+    private String getString(BTreeNode node, String prefix, boolean isTail) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(prefix).append((isTail ? "└── " : "├── "));
+        for (int i = 0; i < node.numberOfKeys(); i++) {
+            Book value = node.getKey(i);
+            builder.append(value.getISBN());
+            if (i < node.numberOfKeys() - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append("\n");
+
+        if (node.children != null) {
+            for (int i = 0; i < node.numberOfChildren() - 1; i++) {
+                BTreeNode obj = node.getChild(i);
+                builder.append(getString(obj, prefix + (isTail ? "    " : "│   "), false));
+            }
+            if (node.numberOfChildren() >= 1) {
+                BTreeNode obj = node.getChild(node.numberOfChildren() - 1);
+                builder.append(getString(obj, prefix + (isTail ? "    " : "│   "), true));
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private ArrayList<Book> getBooks(BTreeNode node) {
+        ArrayList<Book> arrayList = new ArrayList<>();
+
+        if (node != null) {
+            for (int i = 0; i < node.numberOfKeys(); i++) {
+                Book value = node.getKey(i);
+                arrayList.add(value);
+            }
+
+            if (node.children != null) {
+                for (int i = 0; i < node.numberOfChildren() - 1; i++) {
+                    BTreeNode obj = node.getChild(i);
+                    for (Book book:getBooks(obj)) {
+                        arrayList.add(book);
+                    }
+                }
+                if (node.numberOfChildren() >= 1) {
+                    BTreeNode obj = node.getChild(node.numberOfChildren() - 1);
+                    for (Book book:getBooks(obj)) {
+                        arrayList.add(book);
+                    }
+                }
+            }
+        }
+        return arrayList;
     }
 }
